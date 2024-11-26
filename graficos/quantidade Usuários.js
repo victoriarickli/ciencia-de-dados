66586a68eff06257cb972c11959829b966213c75
victoriarickli <160596546+victoriarickli@users.noreply.github.com>
2024-11-26 08:38:05 -0300

    .

--- graficos/quantidade Usuários.js
+++ graficos/quantidade Usuários.js
@@ -1,5 +1,5 @@
 async function quantidadeUsuarios() {
-  const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json '
+  const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
   const res = await fetch(url)
   const dados = await res.json()
   const pessoas_mundo = Object.keys(dados)
