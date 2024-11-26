047433dfddcf0ed0cfd0043054beb7586533b3a8
victoriarickli <160596546+victoriarickli@users.noreply.github.com>
2024-11-26 10:58:36 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='   '
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
 
